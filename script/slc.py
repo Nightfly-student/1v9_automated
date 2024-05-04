21a961d5cb0e596fe12ead9f414515b58c2d14db
Jelle Toonen <jelletoonenprive@gmail.com>
2024-05-04 17:54:15 +0200

    testing

--- script/slc.py
+++ script/slc.py
@@ -30,32 +30,32 @@ def run(region, user):
     # check where to click
 
 
-    moveAndClickToFile("fileButton.JPG", 1, 0.7)
-    moveAndClickToFile("clipboard.png", 2, 0.8)  
+    # moveAndClickToFile("fileButton.JPG", 1, 0.7)
+    # moveAndClickToFile("clipboard.png", 2, 0.8)  
 
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('tab')
-    pyautogui.press('enter')
+    # pyautogui.press('tab')
+    # pyautogui.press('tab')
+    # pyautogui.press('tab')
+    # pyautogui.press('enter')
 
-    time.sleep(2)
+    # time.sleep(2)
 
-    # moveAndClickToFile("import.JPG", 3, 0.5)
+    # # moveAndClickToFile("import.JPG", 3, 0.5)
 
-    print("imported", flush=True)
-    moveAndClickToFile("checkerButton.PNG", 0.5, 0.75) 
-    print("clicked checker", flush=True)
-    moveAndClickToFile("checkerStart.PNG", 1, 0.7)
+    # print("imported", flush=True)
+    # moveAndClickToFile("checkerButton.PNG", 0.5, 0.75) 
+    # print("clicked checker", flush=True)
+    # moveAndClickToFile("checkerStart.PNG", 1, 0.7)
 
-    keepGoing = True
-    while keepGoing:
-        time.sleep(2)
-        found = waitUntilFileButton("finished.JPG")
-        print(found, flush=True)
-        if found:
-            keepGoing = False
-        else:
-            keepGoing = True
+    # keepGoing = True
+    # while keepGoing:
+    #     time.sleep(2)
+    #     found = waitUntilFileButton("finished.JPG")
+    #     print(found, flush=True)
+    #     if found:
+    #         keepGoing = False
+    #     else:
+    #         keepGoing = True
 
     moveAndClickToFile("toAccount.JPG", 1, 0.7) 
 
@@ -63,8 +63,8 @@ def run(region, user):
     time.sleep(1)
 
     moveAndClickToFile("copyInfo.JPG", 1, 0.7)
-    moveAndClickToFile("copyFormat.JPG", 1, 0.7)
-    moveAndClickToFile("exportToCSV.JPG", 1, 0.7)
+    moveAndClickToFileBackwards("copyFormat.JPG", 1, 0.7)
+    moveAndClickToFileBackwards("exportToCSV.JPG", 1, 0.7)
 
     # time.sleep(2)
 
@@ -113,6 +113,29 @@ def waitUntilFileButton (fileName):
     except Exception as e:
         print("Could not find file button", flush=True)
         return False
+
+def moveAndClickToFileBackwards (fileName, sleepTime, confidence=0.8, click=1):
+    filePath = cv2.imread('script/' + fileName)
+
+    try:
+        file = pyautogui.locateCenterOnScreen(filePath,
+            confidence=confidence,
+        )
+    except Exception as e:
+        print("Could not find file button", flush=True)
+        return "Could not find file button"
+    
+    # move first only the y axis then x axis
+    pyautogui.move(file.x, 0, duration=0.5)
+    pyautogui.move(0, file.y, duration=0.5)
+    print(file, flush=True)
+
+    # pyautogui.moveTo(file, duration=0.5)
+    pyautogui.click(file, clicks=click, interval=0.0, button='left', duration=0.2)   
+
+    time.sleep(sleepTime)
+
+    return
 
 run(sys.argv[1], sys.argv[2])
 
