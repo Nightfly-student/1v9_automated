8d15f0ed1d5654af0dccca7d9ecc01a2396ad13f
Jelle Toonen <jelletoonenprive@gmail.com>
2024-05-04 15:40:29 +0200

    .

--- script/slc.py
+++ script/slc.py
@@ -82,6 +82,8 @@ def moveAndClickToFile (fileName, sleepTime, confidence=0.8, click=1):
     # move first only the y axis then x axis
     # pyautogui.move(0, file.y, duration=0.5)
     # pyautogui.move(file.x, 0, duration=0.5)
+    print(file, flush=True)
+
     pyautogui.moveTo(file, duration=0.5)
     pyautogui.click(file, clicks=click, interval=0.0, button='left')   
 
