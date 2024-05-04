cfcb49d057b2eb93748e0e2c4c86f985766da006
Jelle Toonen <jelletoonenprive@gmail.com>
2024-05-04 12:31:43 +0200

    ch

--- script/slc.py
+++ script/slc.py
@@ -11,12 +11,21 @@ def run(region, user):
     print(region, user, flush=True)
     print("Running", flush=True)
     # foucs on the window SLC Checker - Reborn
-    window = pyautogui.getWindowsWithTitle("SLC Checker - Reborn")
+    app = Application().connect(title_re="SLC Checker - Reborn")
 
-    print(window, flush=True)
+    # get the window
 
+    window = app.window(title_re="SLC Checker - Reborn")
+
+    # if no window is found, return an error message
+    if not window.exists():
+        return "No window found"
+    
     # add user to the clipboard
     pyperclip.copy(user)
+
+    # focus on the window
+    window.set_focus()
 
     # press file button and then Load accounts from clipboard
     # check where to click
@@ -25,28 +34,29 @@ def run(region, user):
 
 
     file = pyautogui.locateOnScreen('/images/fileButton.JPG',
-            confidence=0.6
+            confidence=0.6,
+            minSearchTime=5
     )
 
     print(file, flush=True)
 
-    pyautogui.moveTo(file)
+    pyautogui.moveTo(file, duration=1)
     pyautogui.mouseDown()
     pyautogui.click(file)
 
     time.sleep(1)
-
-    pyautogui.mouseUp()
+    
 
     print("clicking on load accounts from clipboard", flush=True)
 
     time.sleep(1)
 
     load = pyautogui.locateOnScreen('/images/clipboard.png',
-            confidence=0.5
+            confidence=0.5,
+            minSearchTime=5
     )
     
-    pyautogui.moveTo(load)
+    pyautogui.moveTo(load, duration=1)
     pyautogui.mouseDown()
     pyautogui.click(load)
 
